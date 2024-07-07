0d92423c61eadf8b451c5463b1c9c9f22c7d183c
Hung Nguyen Pro <htn@Hungs-MacBook-Pro.local>
2024-07-08 04:21:23 +0700

    Change space

--- backend/try_on.py
+++ backend/try_on.py
@@ -1,11 +1,96 @@
+# import requests
+# import json 
+
+# url = "https://levihsu-ootdiffusion.hf.space/queue/join?"
+# session_hash = "u988s9pxmk"
+
+# def upload_file(humanFile, clothesFile):
+#     uploadUrl = "https://levihsu-ootdiffusion.hf.space/upload?upload_id=4za1726butj"
+#     humanPath = clothesPath = None 
+    
+#     files = {'files': humanFile} 
+#     response = requests.post(uploadUrl, files=files)
+#     humanPath = response.json()[0]
+
+#     files = {'files': clothesFile} 
+#     response = requests.post(uploadUrl, files=files)
+#     clothesPath = response.json()[0]
+    
+#     return humanPath, clothesPath
+
+# def clothes_tryon(humanFile, clothesFile):
+#     humanPath, clothesPath = upload_file(humanFile, clothesFile)
+#     payload = {
+#         "data": [
+#             {
+#                 "path": humanPath, # "/tmp/gradio/0dc2e5ca0e97bcd0c46950316bda838a46167da4/meou.png", 
+#                 "url": "https://levihsu-ootdiffusion.hf.space/file=" + humanPath, # "https://levihsu-ootdiffusion.hf.space/file=/tmp/gradio/0dc2e5ca0e97bcd0c46950316bda838a46167da4/meou.png",
+#                 "size": None,
+#                 "mime_type": "image/png",
+#                 "meta": {
+#                     "_type": "gradio.FileData"
+#                 }
+#             },
+#             {
+#                 "path": clothesPath, # "/tmp/gradio/17c62353c027a67af6f4c6e8dccce54fba3e1e43/048554_1.jpg"
+#                 "url":  "https://levihsu-ootdiffusion.hf.space/file=" + clothesPath, #"https://levihsu-ootdiffusion.hf.space/file=/tmp/gradio/17c62353c027a67af6f4c6e8dccce54fba3e1e43/048554_1.jpg",
+#                 "size": None,
+#                 "mime_type": None,
+#                 "is_stream": False,
+#                 "meta": {
+#                     "_type": "gradio.FileData"
+#                 }
+#             },
+#             "Upper-body",
+#             1,
+#             20,
+#             2,
+#             -1
+#         ],
+#         "event_data": None,
+#         "fn_index": 8,
+#         "trigger_id": 42,
+#         "session_hash": session_hash
+#     }
+
+#     response = requests.post(url, json=payload)
+
+#     if response.status_code == 200:
+#         print('Clothes try-on request succeeded')
+
+#         nextUrl = "https://levihsu-ootdiffusion.hf.space/queue/data?session_hash=" + session_hash
+        
+#         nextResponse = requests.get(nextUrl,stream=True)
+        
+#         for line in nextResponse.iter_lines():
+#             if line:
+#                 decoded_line = line.decode('utf-8')
+#                 if decoded_line.startswith('data: '):
+#                     data = decoded_line[len('data: '):]
+#                     try:
+#                         json_data = json.loads(data)
+                        
+#                         # Check for the message type
+#                         if json_data.get('msg') == 'process_completed':
+#                             print('Output image link:')
+#                             print(json_data['output']['data'][0][0]['image']['url'])
+#                             return json_data['output']['data'][0][0]['image']['url']
+                            
+#                     except json.JSONDecodeError:
+#                         print('Failed to decode JSON:', decoded_line)
+
+#     else:
+#         print('Request failed with status code:', response.status_code)
+
+
 import requests
 import json 
 
 url = "https://levihsu-ootdiffusion.hf.space/queue/join?"
-session_hash = "u988s9pxmk"
+session_hash = "c07ams0oufv"
 
 def upload_file(humanFile, clothesFile):
-    uploadUrl = "https://levihsu-ootdiffusion.hf.space/upload?upload_id=4za1726butj"
+    uploadUrl = "https://yisol-idm-vton.hf.space/upload?upload_id=fg5337lne7i"
     humanPath = clothesPath = None 
     
     files = {'files': humanFile} 
@@ -24,7 +109,7 @@ def clothes_tryon(humanFile, clothesFile):
         "data": [
             {
                 "path": humanPath, # "/tmp/gradio/0dc2e5ca0e97bcd0c46950316bda838a46167da4/meou.png", 
-                "url": "https://levihsu-ootdiffusion.hf.space/file=" + humanPath, # "https://levihsu-ootdiffusion.hf.space/file=/tmp/gradio/0dc2e5ca0e97bcd0c46950316bda838a46167da4/meou.png",
+                "url": "https://yisol-idm-vton.hf.space/file=" + humanPath, # "https://levihsu-ootdiffusion.hf.space/file=/tmp/gradio/0dc2e5ca0e97bcd0c46950316bda838a46167da4/meou.png",
                 "size": None,
                 "mime_type": "image/png",
                 "meta": {
@@ -33,7 +118,7 @@ def clothes_tryon(humanFile, clothesFile):
             },
             {
                 "path": clothesPath, # "/tmp/gradio/17c62353c027a67af6f4c6e8dccce54fba3e1e43/048554_1.jpg"
-                "url":  "https://levihsu-ootdiffusion.hf.space/file=" + clothesPath, #"https://levihsu-ootdiffusion.hf.space/file=/tmp/gradio/17c62353c027a67af6f4c6e8dccce54fba3e1e43/048554_1.jpg",
+                "url":  "https://yisol-idm-vton.hf.space/file=" + clothesPath, #"https://levihsu-ootdiffusion.hf.space/file=/tmp/gradio/17c62353c027a67af6f4c6e8dccce54fba3e1e43/048554_1.jpg",
                 "size": None,
                 "mime_type": None,
                 "is_stream": False,
@@ -58,7 +143,7 @@ def clothes_tryon(humanFile, clothesFile):
     if response.status_code == 200:
         print('Clothes try-on request succeeded')
 
-        nextUrl = "https://levihsu-ootdiffusion.hf.space/queue/data?session_hash=" + session_hash
+        nextUrl = "https://yisol-idm-vton.hf.space/queue/data?session_hash=" + session_hash
         
         nextResponse = requests.get(nextUrl,stream=True)
         
