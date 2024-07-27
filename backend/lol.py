45fc21b346b6bda010980984d0e721af545f2b2c
Hoa La <lavanhoa41@gmail.com>
2024-07-27 12:35:28 +0700

    idm vton model

---
+++ backend/lol.py
@@ -0,0 +1,57 @@
+{
+    "data": [
+        {
+            "background": {
+                "meta": {
+                    "_type": "gradio.FileData"
+                },
+                "path": "/tmp/gradio/4e0eed78ffc0a1eab3d533ee1fa9d8dd47c336f6/background.png",
+                "url": "https://yisol-idm-vton.hf.space/file=/tmp/gradio/4e0eed78ffc0a1eab3d533ee1fa9d8dd47c336f6/background.png",
+                "orig_name": "background.png",
+                "size": 2748159,
+                "mime_type": ""
+            },
+            "layers": [
+                {
+                    "meta": {
+                        "_type": "gradio.FileData"
+                    },
+                    "path": "/tmp/gradio/3958da4d7bca287c0bc4c7bbec26819c22adaab5/layer_0.png",
+                    "url": "https://yisol-idm-vton.hf.space/file=/tmp/gradio/3958da4d7bca287c0bc4c7bbec26819c22adaab5/layer_0.png",
+                    "orig_name": "layer_0.png",
+                    "size": 24043,
+                    "mime_type": ""
+                }
+            ],
+            "composite": {
+                "meta": {
+                    "_type": "gradio.FileData"
+                },
+                "path": "/tmp/gradio/4e0eed78ffc0a1eab3d533ee1fa9d8dd47c336f6/composite.png",
+                "url": "https://yisol-idm-vton.hf.space/file=/tmp/gradio/4e0eed78ffc0a1eab3d533ee1fa9d8dd47c336f6/composite.png",
+                "orig_name": "composite.png",
+                "size": 2748159,
+                "mime_type": ""
+            }
+        },
+        {
+            "meta": {
+                "_type": "gradio.FileData"
+            },
+            "path": "/tmp/gradio/e59dcc4a3bb831f0448dd1e4a01860497eeb2f7a/tshirt.jpg",
+            "url": "https://yisol-idm-vton.hf.space/file=/tmp/gradio/e59dcc4a3bb831f0448dd1e4a01860497eeb2f7a/tshirt.jpg",
+            "orig_name": "tshirt.jpg",
+            "size": 67438,
+            "mime_type": "image/jpeg"
+        },
+        "",
+        true,
+        false,
+        30,
+        42
+    ],
+    "event_data": null,
+    "fn_index": 2,
+    "trigger_id": 25,
+    "session_hash": "8dd1sr4b7ua"
+}
